33cff0d9f2855888d6dfb6204cbe0068e8701969
Richard <au.richard4@gmail.com>
2022-02-22 13:40:45 -0800

    Created delete button for URL's

--- express_server.js
+++ express_server.js
@@ -8,7 +8,8 @@ app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "a9ml61": "http://www.instagram.com"
 };
 
 function generateRandomString() {
@@ -31,37 +32,41 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-
 //Creating New URL
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  let longURL = req.body.longURL;
-  let shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
   urlDatabase[shortURL] = longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
-
+//Deleting URL
+app.post("/urls/:shortURL/delete", (req, res) => {
+  console.log(req);  // Log the POST request body to the console
+  const shortURL = req.params.shortURL;
+  console.log(shortURL);
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-
 //Shortening URL
 app.get("/urls/:shortURL", (req, res) => {
-  let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[shortURL];
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
   const templateVars = { shortURL, longURL };
-  // res.redirect(longURL);
   res.render("urls_show", templateVars);
 });
 
 //Outputting Short URL With Long URL
 app.get("/u/:shortURL", (req, res) => {
-  let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[shortURL];
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
 });
 
